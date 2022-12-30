d939c7518200d9656ba87a4bde0943158cc622e5
Milena <milenka@student.agh.edu.pl>
2022-12-30 19:28:11 +0100

    To do added in playground

--- PO-projekt-zaliczeniowy/src/main/java/org/example/playground.java
+++ PO-projekt-zaliczeniowy/src/main/java/org/example/playground.java
@@ -1,1 +1,12 @@
-//todo
+/*todo:
+
+1. Ogarnac trawe - why nie rysuje sie - both
+2. W reproduction - uniwersalne genotypy (dlugosc) - Milena
+3. Metoda losujaca genotyp - Milena
+4. Licznik do genotypu - Milena
+5. W parameters ograniczenie do ilosci parametrow - Mateusz
+6. Handling parametrow - edge case'y, jak zle to nie dziala :(  - Mateusz
+7. Sledznie 1 zwierzecia - zrobic jako button i set text do statystyk - Mateusz
+8. w GUI ładne kolorki i podpisy (wiek, energia) - Milena
+
+ */
